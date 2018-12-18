556b2c8a6333e55c1cba5ce74e4b48d2f1c42399
Manish Verma <mr.manish4uall@gmail.com>
2018-12-18 12:01:37 +0530

    fixed

--- Invoice/Controllers/HomeController.cs
+++ Invoice/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Invoice.ViewModel;
 
 namespace Invoice.Controllers
 {
@@ -25,6 +26,38 @@ namespace Invoice.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public JsonResult SaveToDb(List<OrderItems> orderItems)
+        {
+            using(OrderManagementDBEntities dbObj = new OrderManagementDBEntities())
+            {
+                Order_mt order = new Order_mt()
+                {
+                    Order_no = orderItems[0].orderNo,
+                    Order_date = orderItems[0].date,
+                    Customer_name = orderItems[0].customerName
+                };
+                dbObj.Order_mt.Add(order);
+                dbObj.SaveChanges();
+
+                var id = dbObj.Order_mt.OrderByDescending(x => x.Order_id).First().Order_id;
+
+                foreach ( var items in orderItems)
+                {                    
+                    Order_dt order_Dt = new Order_dt()
+                    {
+                        Order_id = id,
+                        item = items.itemName,
+                        Quantity = items.quantity,
+                        Rate = items.rate
+                    };
+                    dbObj.Order_dt.Add(order_Dt);
+                    dbObj.SaveChanges();
+                }  
+            }
+
+            return Json(new JsonResult(), JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ Invoice/ViewModel/OrderItems.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Invoice.ViewModel
+{
+    public class OrderItems
+    {
+
+        public string orderNo { get; set; }
+        public DateTime date { get; set; }
+        public string customerName { get; set; }
+        public string itemName { get; set; }
+        public int quantity { get; set; }
+        public decimal rate { get; set; }
+    }
+}
